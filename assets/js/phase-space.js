573a72256f940455aaa0086d0af819b07f142675
phdenzel <phdenzel@gmail.com>
2017-03-07 18:05:30 +0100

    Corrected integrator

--- assets/js/phase-space.js
+++ assets/js/phase-space.js
@@ -6,13 +6,13 @@ var height = canvas.height = window.innerHeight;
 var origin = {x:0.5*width, y:0.5*height};
 var color = '#3DAA77';
 // data
-var N = 1000;
+var N = 100;
 var sheets = [];
 var sheetsBuffer = [];
 var xrange = [0.0, 1.0];
 // physics
 var grav = 0.04;
-var dt = 0.001;
+var dt = 0.005;
 
 // draw coordinate system
 function coordsys(ctx) {
@@ -22,7 +22,7 @@ function coordsys(ctx) {
     ctx.lineWidth = 1;
     ctx.strokeStyle = '#DEDEDE';
     ctx.stroke();
-}
+};
 
 
 // Sheet class
@@ -42,17 +42,14 @@ function DMsheet1d(x, vx, ax=0, m=1) {
     this.kick = function() {
         _this.vx += _this.ax * dt;
     };
-    // complete DKD step
-    this.step = function() {
-        _this.drift();
+    // calculate acceleration for kick step
+    this.potential = function() {
         _this.ax = force1d(_this);
-        _this.kick();
-        _this.drift();
     };
     // draw the sheets as points
     this.draw = function() {
         ctx.fillStyle = color;
-        var xc = mapInterval(_this.x, xrange[0], xrange[1], 0.5*origin.x, 1.5*origin.x);
+        var xc = mapInterval(_this.x, xrange[0], xrange[1], 0.25*origin.x, 1.75*origin.x);
         var yc = mapInterval(_this.vx, 10.0, -10.0, 0, height);
         // remember that we're in phase-space
         ctx.fillRect(xc, yc, size, size);
@@ -63,9 +60,9 @@ function DMsheet1d(x, vx, ax=0, m=1) {
 // m1 * a1 = G * m1 * m2 * sgn(x2-x1)
 function force1d(sheet) {
     // calculate the force of sheet from sheets at index
-    // index = sheets.findIndex(sheet);
     var a = 0;
     for (var i=0; i<N; i++) {
+        // sign(0) should be 0
         a += grav*sheets[i].m*Math.sign(sheets[i].x-sheet.x);
     };
     return a;
@@ -81,25 +78,43 @@ function createSheets(i) {
     var x = mapInterval(i, 0, N, xrange[0], xrange[1]);
     var s = new DMsheet1d(x, 0.0);
     sheets.push(s);
-}
+};
 
 // initiate sheets
 function initiateSheets() {
     for (var i=0; i<N; i++) {
-        //setTimeout(createSheets, 1, i);
         createSheets(i);
+    };
+};
+
+// evolves an array of sheets
+function evolve(sheets) {
+    // Drift
+    for (var i=0; i<N; i++) {
+        sheets[i].drift();
+    };
+    // 1D Poisson equation
+    for (var i=0; i<N; i++) {
+        sheets[i].potential();
+    };
+    // Kick and Drift
+    for (var i=0; i<N; i++) {
+        sheets[i].kick();
+    };
+    for (var i=0; i<N; i++) {
+        sheets[i].drift();
+    };
+    // and project to canvas
+    for (var i=0; i<N; i++) {
+        sheets[i].draw();
     };
 };
 
 // simulate and render
 function render() {
     ctx.clearRect(0, 0, width, height);
-    var sheetsBuffer = Array.from(sheets);
-    for (var i=0; i<N; i++) {
-        // evolve particles
-        sheets[i].step();
-        sheets[i].draw();
-    };
+    // evolve
+    evolve(sheets);
     // log momentum conservation
     var conserv = 0
     for (var i=0; i<N; i++) {
