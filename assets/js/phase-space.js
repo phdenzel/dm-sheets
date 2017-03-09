a689aa7f59726fb06d56a48865e726541fb76601
phdenzel <phdenzel@gmail.com>
2017-03-09 19:36:05 +0100

    Cleanup & axis labels

--- assets/js/phase-space.js
+++ assets/js/phase-space.js
@@ -3,43 +3,60 @@ var canvas = document.getElementById('phase-space');
 var ctx = canvas.getContext('2d');
 var width = canvas.width = window.innerWidth;
 var height = canvas.height = window.innerHeight;
-var origin = {x:0.5*width, y:0.5*height};
+var origin = {x:0.5*width, y:0.5*height}; // canvas origin at top left
+var proj_x = {min:0.5*origin.x, max:1.5*origin.x};
+var proj_y = {min:0.0, max:height};
 var color = '#3DAA77';
-// data
+// data & physics
 var N = 1000;
-var sheets = [];
-var sheetsBuffer = [];
-var xrange = [0.0, 1.0];
-// physics
 var grav = 0.04;
 var dt = 0.005;
+var particles = [];
+var xrange = [0.0, 1.0]; // used for initial conditions
+var vrange = [10.0, -10.0]; // should simply cover the min/max velocity
 
+// linear mapping of value from [A, B] > [a, b]
+function mapInterval(val, A, B, a, b){
+    return (val-A)*(b-a)/(B-A) + a;
+};
+
+// random number
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+};
 
 // draw coordinate system
 function coordsys(ctx) {
+    ctx.strokeStyle = '#DEDEDE';
+    ctx.fillStyle='#DEDEDE';
+    ctx.font="16px Arial";
+    // coordinate lines
     ctx.beginPath();
     ctx.moveTo(0, origin.y); ctx.lineTo(width, origin.y);
-    ctx.moveTo(0, 0); ctx.lineTo(0, height);
+    ctx.moveTo(proj_x.min, 0); ctx.lineTo(proj_x.min, height);
     ctx.lineWidth = 1;
-    ctx.strokeStyle = '#DEDEDE';
     ctx.stroke();
+    // axis labels
+    ctx.fillText('0', proj_x.min-20, origin.y+20); // xmin
+    ctx.fillText(xrange[1].toString(), proj_x.max, origin.y+20) // xmax
+    ctx.fillText(vrange[0].toString(), proj_x.min-28, 20) // ymin
+    ctx.fillText(vrange[1].toString(), proj_x.min-32, height-20) // ymin
 };
 
-
-// Sheet class
-function DMsheet1d(x, vx, ax=0, m=1) {
-    // Constructs a 1D dark matter sheet with its phase-space coordinates
+// Particle class
+function DMParticle1d(x, vx, ax=0, m=1) {
+    // Constructs a 1D dark matter particle with its phase-space coordinates
     var _this = this;
     this.x = x;
     this.vx = vx;
     this.ax = ax;
     this.m = m;
     var size = 5+m;
-    // Drift half-step
+    // drift half-step
     this.drift = function() {
         _this.x += _this.vx * 0.5*dt;
     };
-    // Kick full-step
+    // kick full-step
     this.kick = function() {
         _this.vx += _this.ax * dt;
     };
@@ -47,84 +64,77 @@ function DMsheet1d(x, vx, ax=0, m=1) {
     this.potential = function() {
         _this.ax = force1d(_this);
     };
-    // draw the sheets as points
+    // draw the particles as points
     this.draw = function() {
         ctx.fillStyle = color;
-        var xc = mapInterval(_this.x, xrange[0], xrange[1], 0.25*origin.x, 1.75*origin.x);
-        var yc = mapInterval(_this.vx, 10.0, -10.0, 0, height);
         // remember that we're in phase-space
+        var xc = mapInterval(_this.x, xrange[0], xrange[1],
+                             proj_x.min, proj_x.max);
+        var yc = mapInterval(_this.vx, vrange[0], vrange[1],
+                             proj_y.min, proj_y.max);
         ctx.fillRect(xc, yc, size, size);
     };
 };
 
-// 1-D force from Laplace equation
+// 1D force from Poisson equation
 // m1 * a1 = G * m1 * m2 * sgn(x2-x1)
-function force1d(sheet) {
-    // calculate the force of sheet from sheets at index
+function force1d(p) {
+    // calculate the force that particle p feels
     var a = 0;
     for (var i=0; i<N; i++) {
         // sign(0) should be 0
-        a += grav*sheets[i].m*Math.sign(sheets[i].x-sheet.x);
+        a += grav*particles[i].m*Math.sign(particles[i].x-p.x);
     };
     return a;
 };
 
-// linear mapping of value from [A, B] > [a, b]
-function mapInterval(val, A, B, a, b){
-    return (val-A)*(b-a)/(B-A) + a;
-};
-
-function getRandomArbitrary(min, max) {
-    return Math.random() * (max - min) + min;
-};
-
-// create sheets
-function createSheets(i) {
-    var x = mapInterval(i, 0, N, xrange[0], xrange[1]);
-    var s = new DMsheet1d(x, getRandomArbitrary(0.001, 0.01));
-    sheets.push(s);
-};
-
-// initiate sheets
-function initiateSheets() {
+// initial conditions
+function createParticles(i) {
     for (var i=0; i<N; i++) {
-        createSheets(i);
+        // evenly spaced
+        var x = mapInterval(i, 0, N, xrange[0], xrange[1]);
+        // at rest, i.e. vx=0
+        //var s = new DMParticle1d(x, 0.0);
+        // small random perturbation in vx
+        var p = new DMParticle1d(x, getRandomArbitrary(-0.01, 0.01));
+        particles.push(p);  
     };
 };
 
-// evolves an array of sheets
-function evolve(sheets) {
+// evolves an array of particles
+function evolve(particles) {
     // Drift
     for (var i=0; i<N; i++) {
-        sheets[i].drift();
+        particles[i].drift();
     };
     // 1D Poisson equation
     for (var i=0; i<N; i++) {
-        sheets[i].potential();
+        particles[i].potential();
     };
     // Kick
     for (var i=0; i<N; i++) {
-        sheets[i].kick();
+        particles[i].kick();
     };
     // Drift
     for (var i=0; i<N; i++) {
-        sheets[i].drift();
+        particles[i].drift();
     };
     // and project to canvas
     for (var i=0; i<N; i++) {
-        sheets[i].draw();
+        particles[i].draw();
     };
 };
 
 // simulate and render
 function render() {
+    // clear screen
     ctx.clearRect(0, 0, width, height);
-    // evolve
-    evolve(sheets);
-    // log momentum conservation
+    // evolve and redraw
+    evolve(particles);
+    // log total momentum
     var conserv = 0
     for (var i=0; i<N; i++) {
-        conserv += sheets[i].vx;
+        conserv += particles[i].vx;
     }
     console.log(conserv);
     // redraw coordinate system
@@ -140,5 +150,5 @@ function resize() {
 };
 
 // Main
-initiateSheets();
+createParticles();
 render();
